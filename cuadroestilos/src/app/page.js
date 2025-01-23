b7419e90e97a5ab750d3b082239c4ceee006ad4b
T3d4xjr <tedaxjr@gmail.com>
2025-01-23 22:34:39 +0100

    UPD:Preguntar a nazaret sobre la transformacion

---
+++ cuadroestilos/src/app/page.js
@@ -0,0 +1,12 @@
+import "@/app/styles/global.css";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="vistaEspalda"></div>
+      <div className="vistaFrente"></div>
+      <div className="interactivo" ></div>
+    </div>
+  );
+}
+
